a63a4f531df8400cc829e846bd206ddd307d82f9
tkellerer <tkellerer@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2018-11-20 19:22:35 +0000

    git-svn-id: http://sqlworkbench.mgm-tp.com/sqlworkbench/trunk/sqlworkbench@7923 6c36a7f0-0884-4472-89c7-80b7fa7b7f28

--- src/workbench/gui/dbobjects/TableDataPanel.java
+++ src/workbench/gui/dbobjects/TableDataPanel.java
@@ -37,7 +37,6 @@ import java.sql.SQLException;
 import java.sql.Savepoint;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
